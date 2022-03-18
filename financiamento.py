e4c7ac0e0fb16b12da0f5f67dd02e6a8f2b6524b
MatheusFelipeLima <100323046+MatheusFelipeLima@users.noreply.github.com>
2022-03-17 23:24:21 -0300

    Update financiamento.py

--- financiamento.py
+++ financiamento.py
@@ -1,12 +1,12 @@
-Casa=float(input('digite o valor do imovelR$:'))
-Salario=float(input('digite o valor do salario R$:))
+Casa =float(input('digite o valor do imovelR$:'))
+Salario=float(input('digite o valor do salario R$:'))
 Tempo=int(input('digite o total de anos, que pretende parcelar R$'))
 #Preciso converter o tempo de anos em meses
-#verificar se o mensal nao excede o valor de 30% do salario 
+#verificar se o mensal nao excede o valor de 30% do salario
 #dizer se aprova ou nao o financiamento
-parcela= Casa/(Tempo*12)
+parcela = Casa / (Tempo*12 )
 #SE salario for menor que 30%, NAO APROVA
 if parcela>(Salario*30/100):
-    print('seu financiamento nao foi aprovado devido que as parcelas excedem 30% do seu salario' )
-else :
- print('seu financiamento foi aprovado')
+    print(f'seu financiamento nao foi aprovado devido que as parcelas são {parcela:.2f} e excedem 30% do seu salario' )
+else:
+ print(f'seu financiamento foi aprovado, as parcelas serão de {parcela:.2f}')
